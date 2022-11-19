59e1d84cfbd36a58ebadfbd3e2a05e0aad2b887a
VadimKorobec <_s_e_t__@ukr.net>
2022-11-19 16:42:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './css/styles.css';
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
 
 const DEBOUNCE_DELAY = 300;
+
+function createMarcupCard(arr) {
+  const markup = arr
+    .map(
+      item => `<img src="${item.flags.svg}" alt=""/>
+  <h1 class="titel">${item.name}</h1>
+  <div>
+  <div class="field">
+  <span class="label">Capital:</span>
+  <span class="value" data-capital>${item.capital}</span>
+  </div>
+  <div class="field">
+  <span class="label">Population:</span>
+  <span class="value" data-population>${item.population}</span>
+  </div>
+  <div class="field">
+  <span class="label">Languages:</span>
+  <span class="value" data-languages>${languages
+    .map(({ name }) => `<span>${name}</span>`)
+    .join(', ')}</span>
+  </div>
+  </div>`
+    )
+    .join('');
+  refs.list.innerHTML = markup;
+}
