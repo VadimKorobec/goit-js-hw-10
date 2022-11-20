0811802ce12b2ad0f18c080d210d3308cb240d80
VadimKorobec <_s_e_t__@ukr.net>
2022-11-20 15:16:44 +0200

    Delete fetchCountries.js

--- fetchCountries.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name/all';
-const fields = 'fields=name,capital,population,flags,languages';
-
-export function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}?${fields}`)
-    .then(response => response.join())
-    .catch(error => console.log(error));
-}
