30296f8cb5929b471c565c53d757373a232d9ab1
VadimKorobec <_s_e_t__@ukr.net>
2022-11-20 14:52:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,85 @@
 import './css/styles.css';
-import { Notify } from 'notiflix';
+import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-import fetchCountries from '../fetchCountries.js';
-
-const refs = {
-  input: document.querySelector('#search-box'),
-  list: document.querySelector('.country-list'),
-  info: document.querySelector('.country-info'),
-};
+import { fetchCountries } from '../fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 
-// fetchCountries();
+const refs = {
+  countruInput: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
 
-function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com/v3.1/name/all';
+refs.countruInput.addEventListener(
+  'input',
+  debounce(onCauntryInput, DEBOUNCE_DELAY)
+);
 
-  fetch(
-    `${BASE_URL}?fields=&name.official=${item.name}&capita=${item.capital}l&population=${item.population}&flags.svg=${item.flags.svg}&languages=${languages}`
-  );
+function onCauntryInput() {
+  const nameCountry = refs.countruInput.value.trim();
+  if (nameCountry === '') {
+    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '');
+  }
+
+  fetchCountries(name)
+    .then(countries => {
+      refs.countryList.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
+      if (countries.length === 1) {
+        refs.countryList.insertAdjacentHTML(
+          'beforeend',
+          marcupCountryList(countries)
+        );
+        refs.countryInfo.insertAdjacentHTML(
+          'beforeend',
+          marcupCountryInfo(countries)
+        );
+      } else if (countries.length >= 10) {
+         alertTooManyMatches();
+      } else {
+        refs.countryList.insertAdjacentHTML(
+          'beforeend'
+           marcupCountryList(countries)
+        );
+      }
+    })
+    .catch(alertWrongName);
 }
 
-function createMarcupCard(arr) {
-  const markup = arr
-    .map(
-      item => `<img src="${item.flags.svg}" alt=""/>
-  <h1 class="titel">${item.name}</h1>
-  <div>
-  <div class="field">
-  <span class="label">Capital:</span>
-  <span class="value" data-capital>${item.capital}</span>
-  </div>
-  <div class="field">
-  <span class="label">Population:</span>
-  <span class="value" data-population>${item.population}</span>
-  </div>
-  <div class="field">
-  <span class="label">Languages:</span>
-  <span class="value" data-languages>${languages
-    .map(({ name }) => `<span>${name}</span>`)
-    .join(', ')}</span>
-  </div>
-  </div>`
-    )
+function marcupCountryList(countries) {
+  const marcup = countries
+    .map(({ name, flags }) => {
+      return `<li class="country-list__item" >
+    <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px/>
+    <h2 class="country-list__name">${name.official}</h2>
+    <li/>`;
+    })
     .join('');
-  refs.list.innerHTML = markup;
+  return marcup;
+}
+
+function marcupCountryInfo(countries) {
+  const marcup = countries
+    .map(({ capital, population, languages }) => {
+      return `<ul class="countri-info__list">
+    <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+    <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+    <li class="country-info__item"><p><b>Languages: </b>${Object.values(
+      languages
+    ).join(', ')}</p></li>
+    </ul>`;
+    })
+    .join('');
+  return marcup;
+}
+
+function alertWrongName() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+
+function alertTooManyMatches() {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
 }
